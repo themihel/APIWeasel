2f311750e99c256e389108fe7631b5595d69205c
themihel <themihel@users.noreply.github.com>
2015-10-24 11:37:05 +0200

    Remove unused code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,26 +5,4 @@
   angular
     .module('apiweasel', []);
 
-  // controller
-  angular
-    .module('apiweasel')
-    .controller('apiWeaselCtrl', ['$scope', '$http', function($scope, $http) {
-      $scope.apiData = [];
-      // fetch data
-
-      // handle success
-      function fetchSuccess(response) {
-        $scope.apiData = response.data;
-      }
-      // handle failure
-      function fetchFailed(response) {
-        console.log('Fail!');
-        console.log(response.data);
-      }
-      // request
-      return $http.get('api.json')
-        .then(fetchSuccess)
-        .catch(fetchFailed);
-    }]);
-
 }());
