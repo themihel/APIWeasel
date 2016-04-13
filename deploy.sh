74c14fbf620b8c2b9e346306e67763a9c8349b64
Mischa Helfenstein <mischa@Mischa-MBP.fritz.box>
2016-04-13 12:18:19 +0200

    Change build process

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  touch dist/.surgeignore && echo '!bower_components/' > dist/.surgeignore;
-  surge --project ./dist --domain apiweasel.themihel.me
-fi
